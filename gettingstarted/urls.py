d4c380e036f894e60ddc49e0300a2825590e3e48
Mihai <mihaivisuian@gmail.com>
2017-03-19 04:10:44 +0000

    fsd

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^response/(?P<username>\w{0,50})/$', hello.views.response, name='user'),
+    url(r'^response(?P<username>\w{0,50})/$', hello.views.response),
 ]
