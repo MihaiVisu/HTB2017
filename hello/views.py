45b0b9a19266bb8680c976bab00cbc0b7059ee99
Mihai <mihaivisuian@gmail.com>
2017-03-19 03:15:38 +0000

    add new request

--- hello/views.py
+++ hello/views.py
@@ -18,3 +18,18 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
+
+def response(request):
+    sid = SentimentIntensityAnalyzer()
+
+    response = urllib2.urlopen("https://arduino-sticky-notes.herokuapp.com/twitter/realdonaldtrump").read()
+
+    ss = sid.polarity_scores(response)
+    mx = -1
+    res = "neu"
+    for k in ss:
+        if ss[k] > mx and k != "compound":
+            mx = ss[k]
+            res = k
+
+    return response + "\n" + res
